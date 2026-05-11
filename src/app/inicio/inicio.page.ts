e3c4a7c25b652e918a11db0e66ddf0f581e4f8b8
Jose-Antonio-San <jsanmor721.ieslapuebla@gmail.com>
2026-05-11 16:34:33 +0200

    Segundo Commit Sprint 2
    Segundo Commit

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  ModalController,
+  ToastController,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
+import { AddTaskModalComponent } from '../components/add-task-modal/add-task-modal.component';
+import { TaskService } from '../models/services/task.service';
 
 @Component({
   selector: 'app-tab1',
@@ -10,7 +30,33 @@ import { add } from 'ionicons/icons';
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon],
 })
 export class Tab1Page {
-  constructor() {
+  constructor(
+    private taskService: TaskService,
+    private modalCtrl: ModalController,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) {
     addIcons({ add });
+  }
+
+  async openAddModal() {
+    const modal = await this.modalCtrl.create({ component: AddTaskModalComponent });
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+
+    if (data) {
+      this.taskService.addTask({ ...data, completed: false });
+
+      const toast = await this.toastCtrl.create({
+        message: '✅ Tarea creada correctamente',
+        duration: 2000,
+        position: 'bottom',
+        color: 'success',
+      });
+      await toast.present();
+
+      await this.router.navigate(['/tabs/tab2']);
+    }
   }
 }
